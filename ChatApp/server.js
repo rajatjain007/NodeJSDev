4b7016f218b65260ec4b81c9242af33e620630ea
Rajat Jain <rajat.apple007@gmail.com>
2022-04-04 20:20:52 -0700

    Get and Post APIs for messages.

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var messages = [{name:"Rajat",message:"GG"},{name:"Jain",message:"Slaughter Gang!"}]
+//Gettting messages
+app.get('/messages',(req,res)=>{
+    res.send(messages);
+})
+
+//Posting messagin
+app.post('/messages',(req,res)=>{
+    messages.push(req.body);
+    res.sendStatus(200);
+})
+
+
 var server = app.listen(3000,()=>{
     console.log('Server listening on port',server.address().port);
 });
