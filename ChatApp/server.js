2c414c46e67ba6d307adf4bb43c92b70b2ffe5a8
Rajat Jain <rajat.apple007@gmail.com>
2022-04-05 20:26:11 -0700

    Chat functionality with MongoDB

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -3,27 +3,54 @@ var app = express();
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+const { sendStatus } = require('express/lib/response');
+require('dotenv').config();
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-var messages = [{name:"Rajat",message:"GG"},{name:"Jain",message:"Slaughter Gang!"}]
+var dbUrl = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster1.xjmhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// Message
+var Message = mongoose.model('Message',{
+    name:String,
+    message:String
+})
+
+var messages = []
 //Gettting messages
 app.get('/messages',(req,res)=>{
-    res.send(messages);
+    Message.find({},(err,messages)=>{
+        if(err){
+            sendStatus(500);
+        }
+        res.send(messages);
+    })
 })
 
 //Posting messages
 app.post('/messages',(req,res)=>{
-    messages.push(req.body);
-    io.emit('message',req.body)
-    res.sendStatus(200);
+    var message = new Message(req.body)
+    message.save((err)=>{
+        if(err){
+            sendStatus(500);
+        }
+        messages.push(req.body);
+        io.emit('message',req.body)
+        res.sendStatus(200);
+    })
 })
 
 io.on('connection',(socket)=>{
     console.log("user connected...");
 })
+
+mongoose.connect(dbUrl,(err)=>{
+    console.log('MongoDB error:',err);
+})
+
 var server = http.listen(3000,()=>{
     console.log('Server listening on port',server.address().port);
 });
